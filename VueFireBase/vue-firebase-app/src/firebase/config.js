bd6918dca389c303f1085b6010e716b4875d325d
Yücel AYDIN <98191391+Ly0403@users.noreply.github.com>
2023-03-09 15:17:32 +0300

    Update config.js

--- VueFireBase/vue-firebase-app/src/firebase/config.js
+++ VueFireBase/vue-firebase-app/src/firebase/config.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCbqCCElvt4e1QHffEgbDA21UMLrlHeVJE",
-  authDomain: "vuetest-deea1.firebaseapp.com",
-  projectId: "vuetest-deea1",
-  storageBucket: "vuetest-deea1.appspot.com",
-  messagingSenderId: "551129722563",
-  appId: "1:551129722563:web:6561610f5a198c4cc97c76",
-  measurementId: "G-VBHZ368CKY"
-};
+const firebaseConfig = {};
 
 const fb = initializeApp(firebaseConfig);
 
