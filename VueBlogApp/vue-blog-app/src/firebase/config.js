29a3099c4400c7a91be2c94899184fea60f1feb8
Yücel AYDIN <98191391+Ly0403@users.noreply.github.com>
2023-03-09 15:18:06 +0300

    Update config.js

--- VueBlogApp/vue-blog-app/src/firebase/config.js
+++ VueBlogApp/vue-blog-app/src/firebase/config.js
@@ -2,13 +2,6 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWruwUrSipIuL25ex58p-0kF8za94OVoY",
-  authDomain: "vue-blog-app-cce84.firebaseapp.com",
-  projectId: "vue-blog-app-cce84",
-  storageBucket: "vue-blog-app-cce84.appspot.com",
-  messagingSenderId: "577780592124",
-  appId: "1:577780592124:web:c15961e7bfc04f1b2f9679",
-  measurementId: "G-VQZRNLTLM0"
 };
 
 
